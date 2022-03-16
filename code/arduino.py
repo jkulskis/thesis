8eba64511a9154ed6e1c4255fa0b376d5784f1ba
John Mikulskis <jkulskis@bu.edu>
2022-03-16 18:59:15 -0400

    arduino command and communication framework

---
+++ code/arduino.py
@@ -0,0 +1,105 @@
+import serial
+import time
+from collections import deque
+from typing import List, Deque
+from enum import Enum
+
+END_CHAR = "#"
+
+
+class Device(Enum):
+    turntable = 0
+    imu = 1
+
+
+class ArdCommand:
+    def __init__(
+        self,
+        device_name: str,
+        instruction: str,
+        ack_timeout: float = 0.0,
+    ):
+        """
+
+        Args:
+            device_name (str): Must be a device defined in the Device class
+            instruction (str): Instruction to send over to the device
+            ack_timeout (float, optional): Wait for an ack for this amount of time in seconds. If 0, not expecting an ack
+        """
+        try:
+            self.device = Device[device_name.lower()]
+        except KeyError:
+            raise ValueError(f"Unknown Device: {device_name}")
+        self.instruction = instruction
+        self.ack_timeout = ack_timeout
+        self.ack = None
+
+    def __repr__(self):
+        return f"{self.device.value}_{self.instruction}_{1 if self.ack_timeout else 0}{END_CHAR}"
+
+
+class TurntableCommand(ArdCommand):
+    def __init__(
+        self,
+        direction: int,
+        speed: int,
+        turn_time: float,
+        ack_timeout: float = 0.0,
+    ):
+        """_summary_
+
+        Args:
+            direction (int): 0 for CW, 1 for CCW
+            speed (int): PWM value to send, should be in the range [0, 255]
+            turn_time (int): Amount of time that we should turn at this speed and direction
+            ack_timeout (float, optional): Wait for an ack for this amount of time in seconds. If 0, not expecting an ack
+        """
+        if speed > 255 or speed < 0:
+            raise ValueError(f"Invalid speed value: {speed}. Must be in range [0, 255]")
+        super().__init__(
+            device_name="turntable",
+            instruction=f"{direction}-{speed}-{turn_time}",
+            ack_timeout=ack_timeout,
+        )
+
+
+class Arduino:
+    def __init__(self, port="/dev/ttyACM0", timeout=5, baudrate=9600):
+        self.ard = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+        self.commands: Deque[ArdCommand] = deque()
+        self.command_history: List[ArdCommand] = []
+
+    def add_command(self, command: ArdCommand):
+        self.commands.append(command)
+
+    def send_command(self, command: ArdCommand = None):
+        if command is None and not self.commands:
+            raise ValueError(
+                "Need to pass a command as a param or add a command to the queue"
+            )
+        # priority to command passed as param, otherwise pop from the command queue
+        command_to_send = command if command is not None else self.commands.popleft()
+        ard.flush()
+        ard.write(str(command).encode())
+        send_time = time.time()
+        if command.ack_timeout:  # only expecting ack if we have a timeout > 0
+            prev_timeout = self.ard.timeout
+            self.ard.timeout = command.ack_timeout
+            command.ack = self.ard.read_until(
+                expected=END_CHAR
+            )  # read until we see the end char or until we timeout
+            self.ard.timeout = prev_timeout  # reset back to old timeout rather than command specific ack timeout
+        self.command_history.append(command)
+    
+    def read(self, timeout=0):
+        """Read until the end character if timeout is 0, otherwise read until timeout
+        """
+        if timeout:
+            return self.ard.read(timeout=timeout)
+        return self.ard.read_until(expected=END_CHAR)
+
+
+if __name__ == "__main__":
+    # testing purposes
+    print(ArdCommand(device_name="IMU", instruction="dummy-instruction"))
+    print(TurntableCommand(direction=0, speed=20, turn_time=2.3))
